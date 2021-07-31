67aee6349c85f931a6007cbaff84b7db923ba6ce
yash <yash.upadhyay@deepcognition.ai>
2021-07-31 22:50:44 +0530

    inferencing feature extractor for every split

--- run_wav2vec.py
+++ run_wav2vec.py
@@ -3,22 +3,23 @@ import fairseq
 import torchaudio
 
 
-from utils import split_audio
+from utils import split_audio , load_splitted_audio
 
 model_path = r'D:\interviews\fairseq\weights\wav2vec_large.pt'
-f_name = r'D:\interviews\fairseq\audio_file\splits\1.flac'
-
-split_audio(f_name,split_length=78960*3,output_path=r'D:\interviews\fairseq\audio_file\splits')
+f_name = r'D:\interviews\fairseq\audio_file\yash.flac'
+output_path = r'D:\interviews\fairseq\audio_file\splits'
+split_audio(f_name,split_length=78960*3,output_path=output_path)
+audio_tensors = load_splitted_audio(output_path)
 
 model, cfg, task = fairseq.checkpoint_utils.load_model_ensemble_and_task([model_path])
 model = model[0]
 model.eval()
 
-waveform, sample_rate = torchaudio.backend.soundfile_backend.load(f_name)
-
-
 print("running feature extractor")
-z = model.feature_extractor(waveform)
-c = model.feature_aggregator(z)
-print(c)
+
+for i,at in enumerate(audio_tensors):
+    z = model.feature_extractor(at)
+    c = model.feature_aggregator(z)
+    print(c)
+
 
--- utils.py
+++ utils.py
@@ -1,15 +1,18 @@
 import soundfile as sf 
 import os
-import torchaudio 
+import torchaudio as ta
+import glob 
 
 
 def split_audio(path_to_audio,split_length,output_path):
+
+    os.makedirs(output_path,exist_ok=True)
     wave, sample_rate = sf.read(path_to_audio)
     wave1 = wave[:,0] 
     wave1 = wave1.reshape(len(wave1))
     counter = 0
     for i in range(0,len(wave1),split_length):
-        if (i+split_length) < len(len(wave1)):
+        if (i+split_length) < len(wave1):
             wave_split = wave1[i:i+split_length]
         else:
             wave_split = wave1[i:]
@@ -20,5 +23,10 @@ def split_audio(path_to_audio,split_length,output_path):
 
 
 def load_splitted_audio(path):
-    return 0
+    audios = glob.glob(f"{path}/*.flac")
+    list_of_audio_tensors = []
+    for a in audios:
+        waveform, sample_rate = ta.backend.soundfile_backend.load(a)
+        list_of_audio_tensors.append(waveform)
+    return list_of_audio_tensors
 
